76e62d4ed1b06a4fd86a251c420564a98d5d7792
pikaqqqqq <281705236@qq.com>
2017-07-30 10:11:01 +0800

    //完成list&toolbar menu的监听，准备加入chatUI

--- app/src/main/java/com/example/android/tourlink/activity/RadioGroupTabActivity.java
+++ app/src/main/java/com/example/android/tourlink/activity/RadioGroupTabActivity.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.android.tourlink.R;
 import com.example.android.tourlink.buttom_navigation.DataGenerator;
@@ -27,6 +29,30 @@ public class RadioGroupTabActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.message_toolbar_menu);
         toolbar.setTitle(R.string.home_page);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int menuItemId = item.getItemId();
+                if (menuItemId == R.id.action_search) {
+                    Toast.makeText(getApplication(), R.string.menu_search, Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_add_friend) {
+                    Toast.makeText(getApplication() , R.string.menu_add_friend , Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_add_group) {
+                    Toast.makeText(getApplication() , R.string.menu_add_group , Toast.LENGTH_SHORT).show();
+
+                } else if (menuItemId == R.id.action_creat_group) {
+                    Toast.makeText(getApplication() , R.string.menu_creat_group , Toast.LENGTH_SHORT).show();
+
+                }else if (menuItemId == R.id.action_rich_scan) {
+                    Toast.makeText(getApplication() , R.string.menu_rich_scan , Toast.LENGTH_SHORT).show();
+
+                }
+                return true;
+            }
+        });
 
         initView();
     }
--- app/src/main/java/com/example/android/tourlink/buttom_navigation/fragment/DiscoveryFragment.java
+++ app/src/main/java/com/example/android/tourlink/buttom_navigation/fragment/DiscoveryFragment.java
@@ -22,6 +22,7 @@ public class DiscoveryFragment extends Fragment {
     private String mFrom;
 
     private ListItemView attributeGroupSetting;
+    private ListItemView attributeLittleNine;
 
     public static DiscoveryFragment newInstance(String from){
         DiscoveryFragment fragment = new DiscoveryFragment();
@@ -50,17 +51,26 @@ public class DiscoveryFragment extends Fragment {
 //        textView.setText(mFrom);
 //        content.setText("群组");
         attributeGroupSetting = (ListItemView)view.findViewById(R.id.list_item_group_view);
+        attributeLittleNine = (ListItemView)view.findViewById(R.id.list_item_user2);
+
         attributeGroupSetting.setOnClickListener(new ListItemView.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(),"组织架构", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(),"从你的全世界路过", Toast.LENGTH_SHORT).show();
             }
         });
-        attributeGroupSetting.setOnMenuItemClickListener(new ListItemView.OnMenuItemClickListener(){
+        attributeGroupSetting.setOnMenuItemClickListener(new ListItemView.OnMenuItemClickListener() {
             @Override
             public void onActionMenuItemSelected(MenuItem item) {
-                Toast.makeText(getActivity(),"设置", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "设置", Toast.LENGTH_SHORT).show();
+            }
+        });
+        attributeLittleNine.setOnClickListener(new ListItemView.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"小阿九", Toast.LENGTH_SHORT).show();
             }
         });
         return view;
